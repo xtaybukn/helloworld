ece95b6edd0406f7ad33830325686bf9bba4e16a
xtaybukn <zclsky.love@qq.com>
2020-02-01 15:01:51 +0800

    111

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,6 +3,6 @@ package helloworld;
 public class HelloWorld {
  
     public static void main(String[] args) {
-        System.out.println("Hello World 1234");
+        System.out.println("Hello World 12345");
     }
 }
